866b78afb5711ed8b992e113e4c10253c60ebe2a
khvorosttt <khvorosttt@yandex.by>
2024-03-04 15:22:50 +0300

    add implementation method fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -204,8 +204,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const newObj = Object.create(proto);
+  Object.assign(newObj, JSON.parse(json));
+  return newObj;
 }
 
 /**
